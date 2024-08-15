45ed28004b5fd732427a0f05f17d1331c589e9ce
Shady Ahmed <shady.abdullah@bosta.co>
2024-08-15 19:53:05 +0300

    Initial Commit

---
+++ iOSTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTask
+//
+//  Created by Shady on 15/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
